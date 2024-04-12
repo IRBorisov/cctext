e7331dd34e8f48d38b508a16b333aa33a08c32bc
IRBorisov <8611739+IRBorisov@users.noreply.github.com>
2024-04-12 13:26:42 +0300

    Fix build and add py.typed

--- scripts/Build.ps1
+++ scripts/Build.ps1
@@ -3,24 +3,25 @@ Set-Location $PSScriptRoot\..
 $packageName = 'cctext'
 $output = 'dist'
 $python = '.\venv\Scripts\python.exe'
-if (-not (Test-Path -Path $python -PathType Leaf)) {
+
+if (-not (Test-Path -Path ${python} -PathType Leaf)) {
     & 'python' -m venv .\venv
-    & $python -m pip install -r requirements-build.txt
+    & ${python} -m pip install -r requirements-build.txt
 }
 
 if (Test-Path -Path $output\$packageName) {
     Remove-Item $output\$packageName -Recurse -Force
 }
 
-& $python -m build --outdir=$output\
-$wheel = Get-Childitem -Path $output\*.whl -Name
-if (-not $wheel) {
-    Write-Error "No wheel generated for $packageName"
+& ${python} -m build --outdir=${output}\
+$wheel = Get-Childitem -Path ${output}\*.whl -Name
+if (-not ${wheel}) {
+    Write-Error "No wheel generated for ${packageName}"
     Exit 1
 }
 
-& $python -m pip install -I $output\$wheel
-& $python -m unittest
+& ${python} -m pip install -I ${output}\${wheel}
+& ${python} -m unittest
 $exitcode = $LASTEXITCODE
-& $python -m pip uninstall -y $packageName
+& ${python} -m pip uninstall -y $packageName
 Exit $exitcode
